9f8827c77a7b2423f126731448d274c87b9a9239
Alexey Ershov <alex4rom@gmail.com>
2016-03-13 17:10:36 +0300

    Add js

---
+++ game.js
@@ -0,0 +1,11 @@
+var stage = document.querySelector('#stage');
+var output = document.querySelector('#output');
+
+var map = [
+			[0, 2, 0, 0, 0, 3],
+			[0, 0, 0, 1, 0, 0],
+			[0, 1, 0, 0, 0, 0],
+			[0, 0, 0, 0, 2, 0],
+			[0, 2, 0, 1, 0, 0],
+			[0, 0, 0, 0, 0, 0],
+		  ];
