9c67beefc727b0d821d3fc63d11af0df9599f317
Alexey Ershov <alex4rom@gmail.com>
2016-03-18 05:13:43 +0300

    Add codes

--- game.js
+++ game.js
@@ -9,3 +9,16 @@ var map = [
 			[0, 2, 0, 1, 0, 0],
 			[0, 0, 0, 0, 0, 0],
 		  ];
+
+var FLOOR = 0;
+var GOLD = 1;
+var HOME = 3;
+
+var ROWS = map.length;
+var COLS = map[0].length;
+
+var SIZE = 64;
+
+render();
+
+//TODO: render
