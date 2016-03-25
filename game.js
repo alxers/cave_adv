b0508b60d291d22f92f3cbb7b0758bfea4dc659a
Alexey Ershov <alex4rom@gmail.com>
2016-03-25 05:11:36 +0300

    Add render

--- game.js
+++ game.js
@@ -2,16 +2,17 @@ var stage = document.querySelector('#stage');
 var output = document.querySelector('#output');
 
 var map = [
-			[0, 2, 0, 0, 0, 3],
-			[0, 0, 0, 1, 0, 0],
-			[0, 1, 0, 0, 0, 0],
-			[0, 0, 0, 0, 2, 0],
-			[0, 2, 0, 1, 0, 0],
-			[0, 0, 0, 0, 0, 0],
-		  ];
+            [0, 2, 0, 0, 0, 3],
+            [0, 0, 0, 1, 0, 0],
+            [0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 2, 0],
+            [0, 2, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+          ];
 
 var FLOOR = 0;
 var GOLD = 1;
+// Exit, not Home?
 var HOME = 3;
 
 var ROWS = map.length;
@@ -22,3 +23,41 @@ var SIZE = 64;
 render();
 
 //TODO: render
+
+function render() {
+
+    // Clear stage of img tag cells
+    // from the previous turn
+    if (stage.hasChildNodes()) {
+        for (var i = 0; i < ROWS * COLS; i++) {
+            stage.removeChild(stage.firstChild) {
+
+            }
+        }
+    }
+
+    // Render the game
+    for (var row = 0; row < ROWS; row++) {
+        for (var col = 0; col < COLS; col++) {
+            var cell = document.createElement('img');
+            cell.setAttribute('class', 'cell');
+            stage.appendChild(cell);
+
+            // Find the correct image for this map cell
+            switch(map[row][col]) {
+                case FLOOR:
+                    cell.src = '../images/floor.png';
+                    break;
+                case GOLD:
+                    cell.src = '../images/gold.png';
+                case HOME:
+                    cell.src = '../images/home.png'
+            }
+
+            // Position the cell
+            cell.style.top = row * SIZE + 'px';
+            cell.style.left = row * SIZE + 'px';
+
+        }
+    }
+}
